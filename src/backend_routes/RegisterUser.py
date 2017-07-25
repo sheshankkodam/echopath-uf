579fad4cdd0a482d8ba8083d3242b1e10665c122
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2017-07-25 16:14:13 -0600

    added validation for names and phone

--- src/backend_routes/RegisterUser.py
+++ src/backend_routes/RegisterUser.py
@@ -5,6 +5,29 @@ from src.ConfigFactory import ConfigFactory
 conf = ConfigFactory.create_app_config()
 
 
+def validate_first_name(val):
+    if not val:
+        raise Exception("first name cannot be empty")
+    if val.isdigit():
+        raise Exception("first name cannot be a number")
+
+
+def validate_last_name(val):
+    if not val:
+        raise Exception("last name cannot be empty")
+    if val.isdigit():
+        raise Exception("last name cannot be a number")
+
+
+def validate_phone_num(val):
+    if not val:
+        raise Exception("phone cannot be empty")
+    if not val.isdigit():
+        raise Exception("phone number cannot be a string")
+    if len(val) != 10:
+        raise Exception("Not a valid number")
+
+
 class RegisterUser(Resource):
     def __init__(self):
         self.__HEADERS = {'Cache-Control': 'private, src-age=0, no-cache', 'Content-type': 'application/json'}
@@ -20,6 +43,10 @@ class RegisterUser(Resource):
         password = request.form['password']
         company_name = request.form['companyName']
         print(first_name, last_name, email, password, phone_number, company_name)
-
-        return 'file uploaded successfully1'
-
+        try:
+            validate_first_name(first_name)
+            validate_last_name(last_name)
+            validate_phone_num(phone_number)
+            return 'Registration successful'
+        except Exception as err:
+            return err.message
