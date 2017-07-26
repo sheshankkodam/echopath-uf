721d1e3f44b940d321507ce88d9ff3a9ef83bb7e
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2017-07-26 12:39:33 -0600

    working notifications

--- src/frontend_static/javascript/register.js
+++ src/frontend_static/javascript/register.js
@@ -1,48 +1,54 @@
 /**
  * Created by sheshank.kodam on 4/15/17.
  */
+
 $(document).ready(function () {
-    $(".btn_submit").click(function(){
-        saveImage();
+    $("#notification-alert").hide();
+    $(".btn_register").click(function(){
+        registerUser();
     });
-
-    $('#success').click(function (e) {
-        e.preventDefault();
-        $('#message').html('<div class="alert alert-success fade in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button>This is a success message</div>');
-    });
-
-    function saveImage() {
-        console.log("Saving image");
-        var firstName = document.getElementsByName("first_name")[0].value;
-        var lastName = document.getElementsByName("last_name")[0].value;
-        var phoneNumber = document.getElementsByName("phone_number")[0].value;
-        var email = document.getElementsByName("email")[0].value;
-        var password = document.getElementsByName("password")[0].value;
-        var companyName = document.getElementsByName("company_name")[0].value;
-
-        var frm = new FormData();
-        frm.append('firstName', firstName);
-        frm.append('lastName', lastName);
-        frm.append('phoneNumber', phoneNumber);
-        frm.append('email', email);
-        frm.append('password', password);
-        frm.append('companyName', companyName);
-
-        $.ajax({
-            type: 'POST',
-            url: '/registerUser',
-            data: frm,
-            contentType: false,
-            cache: false,
-            processData: false,
-            success: function (result) {
-                console.log(result);
-                alert(result)
-            },
-            error: function (error) {
-                console.log(error);
-                alert(error)
-            }
-        })
-    }
 });
+
+function showNotificationAlert(message) {
+    $("#notification-alert").html(message);
+        $("#notification-alert").fadeTo(5000, 500).slideUp(500, function(){
+               $("#notification-alert").slideUp(500);
+                });
+}
+
+function registerUser() {
+    console.log("Saving image");
+    var firstName = document.getElementsByName("first_name")[0].value;
+    var lastName = document.getElementsByName("last_name")[0].value;
+    var phoneNumber = document.getElementsByName("phone_number")[0].value;
+    var email = document.getElementsByName("email")[0].value;
+    var password = document.getElementsByName("password")[0].value;
+    var companyName = document.getElementsByName("company_name")[0].value;
+
+    var frm = new FormData();
+    frm.append('firstName', firstName);
+    frm.append('lastName', lastName);
+    frm.append('phoneNumber', phoneNumber);
+    frm.append('email', email);
+    frm.append('password', password);
+    frm.append('companyName', companyName);
+
+    $.ajax({
+        type: 'POST',
+        url: '/registerUser',
+        data: frm,
+        contentType: false,
+        cache: false,
+        processData: false,
+        success: function (result) {
+            console.log("yuppeee");
+            console.log(result);
+            showNotificationAlert(result)
+        },
+        error: function (error) {
+            console.log("error");
+            console.log(error);
+            showNotificationAlert(error)
+        }
+    });
+}
