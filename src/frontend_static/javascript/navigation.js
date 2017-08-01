2baa9e585efb37d79943ea56e69fb779e3b3bf80
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2017-08-01 17:41:15 -0600

    Added ajax for from and to

--- src/frontend_static/javascript/navigation.js
+++ src/frontend_static/javascript/navigation.js
@@ -4,23 +4,25 @@
 
 var locationsUrl = "http://52.25.29.73:8080/echopath/location/locationsOnly";
 $(document).ready(function () {
+    buildDropDown($('#fromNameId'), 'From?');
+    buildDropDown($('#toNameId'), 'To?');
+
+});
+
+function buildDropDown (dropDownId, emptyMessage) {
     $.ajax({
         type: "GET",
         url: locationsUrl,
         success: function(data)
         {
-            buildDropdown(data, $('#fromNameId'), 'From?');
+            dropDownId.html('');
+            dropDownId.append('<option value="">' + emptyMessage + '</option>');
+            var locs = data["locations"];
+            var value = 1;
+            locs.forEach(function (loc) {
+                dropDownId.append('<option value="' + value + '">' + loc.name + '</option>');
+                value ++
+            });
         }
-    });
-});
-
-function buildDropdown (result, dropdown, emptyMessage) {
-    dropdown.html('');
-    dropdown.append('<option value="">' + emptyMessage + '</option>');
-    var locs = result["locations"];
-    var value = 1;
-    locs.forEach(function (loc) {
-        dropdown.append('<option value="' + value + '">' + loc.name + '</option>');
-        value ++
     });
 }
